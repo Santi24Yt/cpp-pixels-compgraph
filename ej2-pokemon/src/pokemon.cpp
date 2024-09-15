70601f0e14027ca658356d754acda3db0a44349c
Santi24Yt <santi24yt@gmail.com>
2024-09-15 01:17:40 -0600

    lerp chaink

--- ej2-pokemon/src/pokemon.cpp
+++ ej2-pokemon/src/pokemon.cpp
@@ -50,6 +50,14 @@ vector<Point> chaikin(Point p1, Point p2, Point p3, Point p4, int steps) {
     npts.push_back(p4);
     pts = npts;
   }
+  int ptsmax = 1000/(int)pts.size();
+  int ptssizeog = pts.size();
+  for (int i = 0; i < ptssizeog - 1; i++) {
+    for (int t = 0; t < ptsmax; t++) {
+      Point pn = lerp(pts[i], pts[i+1], (double)t/(double)ptsmax);
+      pts.push_back(pn);
+    }
+  }
   return pts;
 }
 
@@ -79,27 +87,27 @@ int main() {
   Point p5 = { 300, 200 };
   Point p6 = { 500, 200 };
   Point p7 = { 700, 400 };
-  vector<Point> ptschaink0 = chaikin(p4, p5, p6, p7, 0);
-  vector<Point> ptschaink1 = chaikin(p4, p5, p6, p7, 1);
-  vector<Point> ptschaink2 = chaikin(p4, p5, p6, p7, 2);
-  vector<Point> ptschaink3 = chaikin(p4, p5, p6, p7, 3);
+  // vector<Point> ptschaink0 = chaikin(p4, p5, p6, p7, 0);
+  // vector<Point> ptschaink1 = chaikin(p4, p5, p6, p7, 1);
+  // vector<Point> ptschaink2 = chaikin(p4, p5, p6, p7, 2);
+  // vector<Point> ptschaink3 = chaikin(p4, p5, p6, p7, 3);
   vector<Point> ptschaink4 = chaikin(p4, p5, p6, p7, 4);
 
   for (Point p : ptschaink4) {
     image[(int)p.y][(int)p.x] = 0x00FFFFFF;
   }
-  for (Point p : ptschaink3) {
-    image[(int)p.y][(int)p.x] = 0xFF00FFFF;
-  }
-  for (Point p : ptschaink2) {
-    image[(int)p.y][(int)p.x] = 0x0000FFFF;
-  }
-  for (Point p : ptschaink1) {
-    image[(int)p.y][(int)p.x] = 0xFF0000FF;
-  }
-  for (Point p : ptschaink0) {
-    image[(int)p.y][(int)p.x] = 0x00FF00FF;
-  }
+  // for (Point p : ptschaink3) {
+  //   image[(int)p.y][(int)p.x] = 0xFF00FFFF;
+  // }
+  // for (Point p : ptschaink2) {
+  //   image[(int)p.y][(int)p.x] = 0x0000FFFF;
+  // }
+  // for (Point p : ptschaink1) {
+  //   image[(int)p.y][(int)p.x] = 0xFF0000FF;
+  // }
+  // for (Point p : ptschaink0) {
+  //   image[(int)p.y][(int)p.x] = 0x00FF00FF;
+  // }
 
 
   byt data[height * width * 4];
